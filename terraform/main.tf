b20e724ad00aed1e0a1328e56b35c270c3cd52e5
netzen86 <netzen0su@gmail.com>
2023-11-10 01:02:24 +0500

    add second vm and lb

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,9 +24,10 @@ resource "yandex_vpc_subnet" "nz-net" {
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
-resource "yandex_container_registry" "nz-registry" {
-  name = "nz-registry"
-}
+
+# resource "yandex_container_registry" "nz-registry" {
+#   name = "nz-registry"
+# }
 
 locals {
   folder_id = "b1g5tv4fsuuk2l9gvd1p"
@@ -76,9 +77,72 @@ resource "yandex_compute_instance" "catgpt-1" {
     }
   }
   metadata = {
-    # docker-compose = file("${path.module}/docker-compose.yaml")
-    user-data = "${file("../../cloud-user.yml")}"
+    docker-compose = file("${path.module}/docker-compose.yaml")
+    user-data = "${file("${path.module}/cloud_config.yaml")}"
+  }
+}
+
+resource "yandex_compute_instance" "catgpt-2" {
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["catgpt-container"].id
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.nz-net.id
+    nat       = true
+  }
+  boot_disk {
+    initialize_params {
+      type     = "network-hdd"
+      size     = "30"
+      image_id = data.yandex_compute_image.coi.id
+    }
+  }
+  metadata = {
+    docker-compose = file("${path.module}/docker-compose.yaml")
+    user-data = "${file("${path.module}/cloud_config.yaml")}"
+  }
+}
+
+resource "yandex_lb_target_group" "nz-tg" {
+  name      = "nz-tg"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.nz-net.id}"
+    address   = "${yandex_compute_instance.catgpt-1.network_interface.0.ip_address}"
+  }
+  target {
+    subnet_id = "${yandex_vpc_subnet.nz-net.id}"
+    address   = "${yandex_compute_instance.catgpt-2.network_interface.0.ip_address}"
   }
 }
 
 
+resource "yandex_lb_network_load_balancer" "nz-lb" {
+  name = "nz-lb"
+  deletion_protection = "false"
+  listener {
+    name = "nz-list"
+    port = 8080
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.nz-tg.id}"
+    healthcheck {
+      name = "nz-tg"
+      http_options {
+        port = 8080
+        path = "/ping"
+      }
+    }
+  }
+}
